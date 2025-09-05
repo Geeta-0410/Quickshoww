71f27b3f9d28d1ba9b72f1734179724fb75892ed
Geeta Chahar <geetachahar43@gmail.com>
2025-09-05 23:15:03 +0530

    Fix User model import path for Vercel

--- server/models/user.js
+++ server/models/user.js
@@ -1,15 +1,12 @@
 import mongoose from "mongoose";
 
+const userSchema = new mongoose.Schema({
+  _id: { type: String, required: true },   // ✅ fixed String
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  image: { type: String, required: true },
+});
 
+const User = mongoose.model("User", userSchema);
 
-const userSchema = new mongoose.Schema(
-  {
- _id:{type:string,required:true},
- name:{type:String,required:true},
- email:{type:String,required:true},
-  image:{type:String,required:true},
-  })
-
-  const User=mongoose.model("User",userSchema);
-
-  export default User;
+export default User;
